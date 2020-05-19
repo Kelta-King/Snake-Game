e61e9767b3cba43d35afd55682b50f54b58789b0
Kushang Shah <kushangshah131@gmail.com>
2020-05-19 11:46:21 +0530

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var game;
+
+game = new Phaser.Game(600, 450, Phaser.AUTO, '');
+
+game.state.add('Menu', Menu);
+game.state.add('Game', Game);
+game.state.add('Game_Over', Game_Over);
+game.state.start('Menu');
