57d042d53741cbd8b6e1e418752e4dfc6b07e032
Kushang Shah <kushangshah131@gmail.com>
2020-05-19 11:51:54 +0530

    Update game_over.js

--- assets/js/game_over.js
+++ assets/js/game_over.js
@@ -4,3 +4,16 @@ var Game_Over = {
 		game.load.image('game_over', './assets/images/gameover.png');
 	},
 	
+	create: function() {
+		this.add.button(0, 0, 'game_over', this.startGame, this);
+		
+		game.add.text(235, 350, "LAST SCORE", { font: "bold 16px sans-serif", fill: "#000", align: "center"});
+        	game.add.text(350, 348, score.toString(), { font: "bold 20px sans-serif", fill: "#000", align: "center" });
+		
+	},
+	
+	startGame: function() {
+		this.state.start('Game');
+	}
+	
+};
